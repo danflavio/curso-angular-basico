702e689cad88c9311b3b7462a13ec0361174e08d
Daniel Flavio <danflavio@gmail.com>
2023-10-10 08:19:41 -0300

    Primeiro Commit

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+  product: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  deleteProduct(): void {
+
+  }
+
+  cancel(): void {
+    
+  }
+
+
+}
